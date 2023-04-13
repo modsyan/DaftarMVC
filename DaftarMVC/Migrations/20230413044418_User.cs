2ceb73c8c331c9b7283656c6545cfffefcc1007e
Muhammed Hamdy <modclover7@gmail.com>
2023-04-13 07:08:23 +0200

    Migrations

---
+++ DaftarMVC/Migrations/20230413044418_User.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DaftarMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class User : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Avatar_link",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BirthDate",
+                table: "Users",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TicketsBilled",
+                table: "Users",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Avatar_link",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "BirthDate",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "TicketsBilled",
+                table: "Users");
+        }
+    }
+}
