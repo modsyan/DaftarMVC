f50009862c9fa8d9786fb3cc2bab02d6b4597995
devBenho <devbenho@gmail.com>
2023-05-15 12:01:20 +0300

    commit from hamdy benho

--- DaftarMVC/Controllers/FavoriteController.cs
+++ DaftarMVC/Controllers/FavoriteController.cs
@@ -1,3 +1,4 @@
+using DaftarMVC.Data;
 using DaftarMVC.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -5,16 +6,47 @@ namespace DaftarMVC.Controllers;
 
 public class FavoriteController : Controller
 {
+    
+    private ApplicationDbContext _applicationDbContext;
+
+    public FavoriteController(ApplicationDbContext applicationDbContext)
+    {
+        _applicationDbContext = applicationDbContext;
+    }
+    
     public IActionResult Index()
     {
-        return View();
+        var userId = HttpContext.Session.GetInt32("UserId");
+        var user = _applicationDbContext.Users.FirstOrDefault(e => e.Id.Equals((userId)));
+        if (user is null)
+            return RedirectToAction("logout", "Account");
+        var favorites = _applicationDbContext.Favorites.Where(e => e.UserId.Equals(user.Id)).ToList();
+        var teachers= new List<Teacher>();
+        foreach (var teacher in favorites.Select(item => _applicationDbContext.Teacher.FirstOrDefault(e=> e.Teacher_Id.Equals(item.TeacherId))))
+        {
+            teachers?.Append(teacher);
+        }
+        return View("Index", teachers);
     }
 
-    public IActionResult AddTeacher()
+    public IActionResult AddTeacher(int teacherId)
     {
-        return View("Index");
+        if (teacherId == null) return RedirectToAction("Index", "Teachers");
+        var userId = HttpContext.Session.GetInt32("UserId");
+        var user = _applicationDbContext.Users.FirstOrDefault(e => e.Id.Equals((userId)));
+        if (user is null)
+            return RedirectToAction("logout", "Account");
+
+        var newFavorite = new Favorite()
+        {
+            UserId = user.Id,
+            TeacherId = teacherId,
+        };
+        _applicationDbContext.Favorites.Add(newFavorite);
+        _applicationDbContext.SaveChanges();
+        return RedirectToAction("Index", "Favorite");
     }
-    public IActionResult RemoveTeacher()
+    public IActionResult RemoveTeacher(int teacherId)
     {
         return View("Index");
     }
